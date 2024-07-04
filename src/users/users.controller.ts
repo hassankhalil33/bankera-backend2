4fb011ca2db53a2fb8b741c9ca5c45271aa64fcc
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-07-04 18:31:40 +0300

    Added Controller Functions to User Model

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, Patch } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getAllUsers() {
+    return {};
+  };
+
+  @Get("/:id")
+  getUser(@Param("id") id: string ) {
+    return { id };
+  };
+
+  @Post()
+  createUser(@Body() body: {name: string, password: string, email: string}) {
+    return body;
+  };
+
+  @Patch("/:id")
+  updateUser(@Param("id") id: string, @Body() body: {name: string, password: string, email: string}) {
+    return {id, ...body};
+  }
+
+  @Delete("/:id")
+  deleteUser(@Param("id") id: string) {
+    return { id };
+  };
+};
