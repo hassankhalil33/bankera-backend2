51dd1dbb041d19c2d758b24b2bbfa28ccc3ab835
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-07-06 02:00:46 +0300

    Created User Services

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Global, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
+@Global()
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: DatabaseService) {}
+
+  async createUser(data) {
+    this.prisma.user.create({
+      data
+    })
+
+    return {"message": "user created successfully"}
+  }
+
+  async getUsers() {
+    return this.prisma.user.findMany()
+  }
+
+  async getUser(username: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        username
+      }
+    })
+  }
+
+  async updateUser(username: string, data) {
+    this.prisma.user.update({
+      data,
+      where: {
+        username
+      }
+    })
+
+    return {"message": "user updated successfully"}
+  }
+
+  async deleteUser(username: string) {
+    this.prisma.user.delete({
+      where: {
+        username
+      }
+    })
+
+    return {"message": "user deleted successfully"}
+  }
+}
