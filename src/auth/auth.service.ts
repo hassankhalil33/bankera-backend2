3c6fdbc51286556d0f3f9c1718993c5d95bc5847
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-07-09 00:22:48 +0300

    Created Basic Registration with Password Hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
+import { RegisterDto } from './dtos/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prisma: DatabaseService) {}
 
-  registerUser(body) {
-    return {"message": "User Registered Successfully", body};
+  async hashPassword(password: string) {
+    const rounds = 15;
+    const salt = "ksadui73dksia8"
+    return await bcrypt.hash(password + salt, rounds);
   };
 
-  loginUser(body) {
+  async registerUser(body: RegisterDto) {
+    const {email, password, username} = body;
+
+    const foundUsername = await this.prisma.user.findUnique({
+      where: {
+        username
+      }
+    });
+
+    if (foundUsername) {
+      throw new BadRequestException("username already exists");
+    } 
+    
+    const foundEmail = await this.prisma.user.findUnique({
+      where: {
+        email
+      }
+    });
+    
+    if (foundEmail) {
+      throw new BadRequestException("email already exists");
+    }
+
+    const hashedPassword = await this.hashPassword(password);
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        ...body,
+        password: hashedPassword,
+        accounts: {
+          create: {}
+        }
+      }
+    });
+
+    return {"message": "User Registered Successfully", newUser};
+  };
+
+  async loginUser(body) {
     return {"message": "User Loggedin Successfully", body};
   };
 
-  logoutUser() {
+  async logoutUser() {
     return {"message": "User Loggedout Successfully"};
   };
 };
