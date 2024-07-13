b08147536fca3ca801805f199af58b5117b43143
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2024-07-13 17:37:57 +0300

    Added Roles Guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,25 +2,26 @@ import { Controller, Get, Param, Body, Delete, Patch, UseGuards, Response } from
 import { UsersService } from './users.service';
 import { AuthGuard } from '@nestjs/passport';
 import { UpdateUserDto } from './dtos/updateuser.dto';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { RoleGuard } from 'src/auth/guards/roles.guard';
 
+@UseGuards(AuthGuard("access"), AuthGuard("refresh"))
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
+  @Roles("ADMIN")
+  @UseGuards(RoleGuard)
   @Get()
   getAllUsers(@Response() res) {
     return this.userService.getUsers(res);
   };
 
-  @UseGuards(AuthGuard("access"))
-  @UseGuards(AuthGuard("refresh"))
   @Get("/:username")
   getUser(@Param("username") username: string, @Response() res ) {
     return this.userService.getUser(res, username);
   };
 
-  @UseGuards(AuthGuard("access"))
-  @UseGuards(AuthGuard("refresh"))
   @Patch("/:username")
   updateUser(@Param("username") username: string, @Body() body: UpdateUserDto) {
     return this.userService.updateUser(username, body);
@@ -31,8 +32,6 @@ export class UsersController {
     return this.userService.deleteAllUsers();
   };
 
-  @UseGuards(AuthGuard("access"))
-  @UseGuards(AuthGuard("refresh"))
   @Delete("/:username")
   deleteUser(@Param("username") username: string) {
     return this.userService.deleteUser(username);
